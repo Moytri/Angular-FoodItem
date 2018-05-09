1d1dde9c8fa0340ad8e20836cc6b898c7e304b6e
moytri <moytritopa@gmail.com>
2018-05-08 23:02:41 -0700

    include FoodItem Class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { AppComponent }   from './app.component';
+//import { FoodItem } from './foodItem';
 import { HomeComponent }        from './home/home.component';
 import { AboutComponent }        from './about/about.component';
 import { routing }        from './app.routing';
@@ -9,7 +10,7 @@ import { routing }        from './app.routing';
 @NgModule({
     imports: [BrowserModule, FormsModule, routing],
     declarations: [AppComponent, 
-        HomeComponent, AboutComponent],
+        HomeComponent, AboutComponent ],
     bootstrap: [AppComponent],
 })
 export class AppModule { }
