e8aad7407e3d98ff5a7224330223cc8d16776726
moytri <moytritopa@gmail.com>
2018-05-08 23:02:40 -0700

    Set session data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { FoodItem } from './foodItem';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,8 +11,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FoodItem';
+
+  myFoodItems: FoodItem[];
+  FOODITEMS: FoodItem[] = [
+    { id: 1, name: "Cucumbers", mfg: "Hot House", pkg: "10 KG", qty: 6 },
+    { id: 2, name: "Carrots", mfg: "Hot House", pkg: "8 KG", qty: 14 },
+    { id: 3, name: "Cheese", mfg: "Hot House", pkg: "5 KG", qty: 20 },
+    { id: 4, name: "French Fries", mfg: "McCain", pkg: "15 KG", qty: 8 }
+  ];
+
+  constructor() {
+    if(sessionStorage.getItem("food") == null) {
+      sessionStorage.setItem("food", JSON.stringify(this.FOODITEMS));
+    }
+
+    this.myFoodItems = JSON.parse(sessionStorage.getItem("food"));
+    
+    //console.log(this.FOODITEMS[0].name);
+  }  
+
+
 }
 
-import { Component } from '@angular/core';
+
 
 
