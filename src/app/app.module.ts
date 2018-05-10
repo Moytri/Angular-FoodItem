7465a568acafdaa4eea948ab281c23db30d0da84
moytri <moytritopa@gmail.com>
2018-05-10 10:24:08 -0700

    few chages has done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { AppComponent }   from './app.component';
-//import { FoodItem } from './foodItem';
+
 import { HomeComponent }        from './home/home.component';
 import { AboutComponent }        from './about/about.component';
 import { routing }        from './app.routing';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, routing],
     declarations: [AppComponent, 
-        HomeComponent, AboutComponent ],
+        HomeComponent, AboutComponent, DetailComponent ],
     bootstrap: [AppComponent],
 })
 export class AppModule { }
