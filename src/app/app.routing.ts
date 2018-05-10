caf2ff186f6f4ed3af6d3eaeecffa36954f4cc63
moytri <moytritopa@gmail.com>
2018-05-10 10:24:08 -0700

    few chages has done

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,14 +5,15 @@ import { Routes, RouterModule }  from '@angular/router';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  
+  { path: 'detail/:id', component: DetailComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
