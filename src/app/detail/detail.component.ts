92935a38d6cefa6f8769e92757f888f6af8a48b7
moytri <moytritopa@gmail.com>
2018-05-10 10:22:20 -0700

    create detalis components css,html,ts  and get data from session

---
+++ src/app/detail/detail.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }  from '@angular/router';
+import { FoodItem } from '../foodItem';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css']
+})
+
+export class DetailComponent implements OnInit {
+
+  id: number;
+  details: FoodItem;
+
+
+  constructor(private route: ActivatedRoute) { 
+
+  }
+
+  ngOnInit() {
+      	this.route.params.forEach((params: Params) => {
+        let localID = params['id'];
+        this.id = localID;
+		let localDeatils = getDetails(this.id);
+		this.details = localDeatils;
+    });
+  }
+
+  	function getDetails(id: number) { 
+
+  		var someFoodItems;
+
+   		if(sessionStorage.getItem("food")) {
+			someFoodItems = JSON.parse(sessionStorage.getItem("food"));
+		}
+
+  		for (var i = 0; i < someFoodItems.length; i++) {  		
+        	if (someFoodItems[i].id == id) { 
+           		 return someFoodItems[i];
+           	} 
+        }
+    }
+
+}
