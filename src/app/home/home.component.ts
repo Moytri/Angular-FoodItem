a56a685a1120ace650731a77b6c8c679df84b5ef
moytri <moytritopa@gmail.com>
2018-05-10 10:24:10 -0700

    few chages has done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,15 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { FoodItem } from '../foodItem';
 
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent  {
 	someFoodItems: FoodItem[];
-
 
 	constructor() {
 		if(sessionStorage.getItem("food")) {
