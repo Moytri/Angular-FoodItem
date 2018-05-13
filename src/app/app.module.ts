9b0fa2854555b4bb5b28e9651c133836b2b919da
moytri <moytritopa@gmail.com>
2018-05-13 13:15:56 -0700

    data binding between ts and html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HomeComponent }        from './home/home.component';
 import { AboutComponent }        from './about/about.component';
 import { routing }        from './app.routing';
 import { DetailComponent } from './detail/detail.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, routing],
     declarations: [AppComponent, 
-        HomeComponent, AboutComponent, DetailComponent ],
+        HomeComponent, AboutComponent, DetailComponent, FooterComponent ],
     bootstrap: [AppComponent],
 })
 export class AppModule { }
